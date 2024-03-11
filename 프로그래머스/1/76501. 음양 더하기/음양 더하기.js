07555325aa627d7152149557bbd396ccc3a60bf7
K <kyungmin727@gmail.com>
2024-03-11 20:03:44 +0900

    [level 1] Title: 음양 더하기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+    for(let i = 0; i<absolutes.length; i++){
+        if(signs[i] === false){
+            absolutes[i] = -absolutes[i]            
+        } else {
+            absolutes[i]
+        }
+    } return absolutes.reduce(function(acc,cur){
+        return acc+cur
+    })
+}
