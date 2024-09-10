9ff20b9c8b9ef2b84c5e25d7cc0d353da3f4d3a9
이경민 <kyungmin727@gmail.com>
2024-09-10 18:14:32 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,10 @@
+function solution(num_list, n) {
+    var answer = [];
+    
+    for(let i=n; i <= num_list.length ; i++){
+        answer.push(num_list[i - 1])
+    }
+    
+    
+    return answer;
+}
