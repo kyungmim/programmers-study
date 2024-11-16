29f302e99b23292ee08abff27ca1e62d0ae7727d
이경민 <kyungmin727@gmail.com>
2024-11-16 23:04:05 +0900

    [level 0] Title: 배열 비교하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,18 @@
+function solution(arr1, arr2) {
+    if (arr1.length > arr2.length) {
+        return 1;
+    } else if (arr1.length < arr2.length) {
+        return -1;
+    }
+    
+    const sum1 = arr1.reduce((acc, num) => acc + num, 0);
+    const sum2 = arr2.reduce((acc, num) => acc + num, 0);
+
+    if (sum1 > sum2) {
+        return 1;
+    } else if (sum1 < sum2) {
+        return -1;
+    }
+
+    return 0;
+}
